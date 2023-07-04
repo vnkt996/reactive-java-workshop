c11cd9211ce350bc3082006da9178bfe23ba3479
saivenkat <saivenkat@vt.edu>
2023-07-04 16:56:49 -0400

    exercise8

--- src/main/java/io/javabrains/reactiveworkshop/Exercise8.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise8.java
@@ -12,14 +12,33 @@ public class Exercise8 {
         // Use ReactiveSources.intNumbersFluxWithException()
 
         // Print values from intNumbersFluxWithException and print a message when error happens
-        // TODO: Write code here
+        ReactiveSources.intNumbersFluxWithException()
+                .subscribe(
+                        x -> System.out.println(x),
+                        err -> System.out.println(err.getMessage())
+                );
+
+//        this doesn't swallow the error
+        ReactiveSources.intNumbersFluxWithException()
+                .doOnError(err -> System.out.println(err.getMessage()))
+                .subscribe(x -> System.out.println(x));
 
         // Print values from intNumbersFluxWithException and continue on errors
-        // TODO: Write code here
+        ReactiveSources.intNumbersFluxWithException()
+                .subscribe(
+                        x -> System.out.println(x),
+                        err -> System.out.println()
+                );
+//        or
+        ReactiveSources.intNumbersFluxWithException()
+                .onErrorContinue((err, item) -> System.out.println(err.getMessage()))
+                .subscribe(x -> System.out.println(x));
 
         // Print values from intNumbersFluxWithException and when errors
         // happen, replace with a fallback sequence of -1 and -2
-        // TODO: Write code here
+        ReactiveSources.intNumbersFluxWithException()
+                .onErrorResume(err -> Flux.just(-1, -2))
+                .subscribe(num -> System.out.println(num));
 
         System.out.println("Press a key to end");
         System.in.read();
